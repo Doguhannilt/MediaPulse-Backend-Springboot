5b82da5db1e2cebb9ef51e0f72d6075aa93af7a7
doguhannilt <doguhannilt@gmail.com>
2024-09-04 09:39:02 +0300

    config added -> BCRYPT

---
+++ src/main/java/com/mediapulse/springboot/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.mediapulse.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/users/**").permitAll()  // Bu endpointlere herkes erişebilir
+                                .anyRequest().authenticated()  // Diğer tüm isteklere kimlik doğrulaması gerektirir
+                );
+        return http.build();
+    }
+}
